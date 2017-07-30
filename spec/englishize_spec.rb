8090a53d252f2616bbbee6fc3e4e1a9e28d7df1d
Alex Shook <alexandra.shook@gmail.com>
2017-07-30 19:06:40 -0400

    Add limit of 1 million

--- spec/englishize_spec.rb
+++ spec/englishize_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../englishize'
 
 describe Englishize do
-
+  describe "LIMIT" do
+    it "should be 1 million" do
+      expect(Englishize::LIMIT).to eq(1_000_000)
+    end
+  end
 end
